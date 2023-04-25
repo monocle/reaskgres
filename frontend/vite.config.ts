36d8585e57526fb08bc851f0662620b810c06610
monocle <monocle.github@gmail.com>
2023-04-25 12:17:09 -0700

    Update vite.config.ts
    Remove usePolling from vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
     host: true,
     strictPort: true,
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://backend:5000',
+      "/api": {
+        target: "http://backend:5000",
         changeOrigin: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
